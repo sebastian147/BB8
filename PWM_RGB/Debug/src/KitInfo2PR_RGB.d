8d2104939c86eeef2f3943d8dd683dbfda58f729
sebastian147 <sebastiandopazo@outlook.com.ar>
2017-08-17 08:55:37 -0300

    Basura

--- PWM_RGB/Debug/src/KitInfo2PR_RGB.d
+++
@@ -1,7 +0,0 @@
-src/KitInfo2PR_RGB.o src/KitInfo2PR_RGB.d: ../src/KitInfo2PR_RGB.c \
- /home/seba-pc/Documentos/utn/info\ 2/Trabajo_practico_Final/PWM_RGB/header/KitInfo2_RGB.h \
- /home/seba-pc/Documentos/utn/info\ 2/Trabajo_practico_Final/PWM_RGB/header/lpc_types.h
-
-/home/seba-pc/Documentos/utn/info\ 2/Trabajo_practico_Final/PWM_RGB/header/KitInfo2_RGB.h:
-
-/home/seba-pc/Documentos/utn/info\ 2/Trabajo_practico_Final/PWM_RGB/header/lpc_types.h:
